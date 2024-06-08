7baa698d6abf8eec0e01bb30f9b14743ec647d33
Saivenkat-18 <kamujussvvenkat@gmail.com>
2024-06-08 08:36:46 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,18 @@
+//To Print Name,DOB,Phone Number
+
+#include <stdio.h>
+
+main()
+{
+    //To Print Name
+    printf("Enter Your Name: RAJ JAIN\n");
+    
+    //To Print DOB
+    printf("Enter Your DOB: Feb 20,2002\n");
+    
+    //To Print Mobile Number
+    printf("Enter Your Mobile Number :9949128444\n");
+    
+}
+
+
